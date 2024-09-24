c8d560444e5919e4316cc1de28ae388229db92c9
AndreyMelman <lagunmelman@gmail.com>
2024-09-24 17:17:36 +0300

    add db helper, add declarative base

---
+++ app/core/models/__init__.py
@@ -0,0 +1,11 @@
+__all__ = (
+    "db_helper",
+    "Base",
+    # "Task",
+    # "Note",
+)
+
+from .db_helper import db_helper
+from .base import Base
+# from .task import Task
+# from .note import Note
