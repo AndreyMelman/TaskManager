e66f3b83ec3bb72aaa0537cb2cf8a12bfdf9fb7e
AndreyMelman <lagunmelman@gmail.com>
2024-09-24 23:37:58 +0300

    add column for note and task

--- app/core/models/note.py
+++ app/core/models/note.py
@@ -1,15 +1,20 @@
-from sqlalchemy import String
+from datetime import datetime
+
+from sqlalchemy import String, func
 from sqlalchemy.orm import Mapped, mapped_column
 
 from .base import Base
-from .mixins import TimestampMixin
 
 
-class Note(Base, TimestampMixin):
+class Note(Base):
 
     title: Mapped[str] = mapped_column(String(50))
     content: Mapped[str]
-
-
-
-
+    created_at: Mapped[datetime] = mapped_column(
+        server_default=func.now(),
+        default=datetime.now,
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        server_default=func.now(),
+        onupdate=func.now()
+    )
--- app/core/models/task.py
+++ app/core/models/task.py
@@ -1,16 +1,23 @@
-from datetime import datetime, UTC
+from datetime import datetime
 
-from sqlalchemy import String, Text
+from sqlalchemy import String, Text, func
 from sqlalchemy.orm import Mapped, mapped_column
 
 from core.models import Base
-from .mixins import TimestampMixin
 
 
-class Task(Base, TimestampMixin):
+class Task(Base):
 
     title: Mapped[str] = mapped_column(String(50))
     description: Mapped[str] = mapped_column(Text)
     priority: Mapped[str] = mapped_column(String(15))
     deadline_at: Mapped[datetime]
     completed: Mapped[bool] = mapped_column(default=False)
+    created_at: Mapped[datetime] = mapped_column(
+        server_default=func.now(),
+        default=datetime.now,
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        server_default=func.now(),
+        onupdate=func.now()
+    )
