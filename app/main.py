7fe53e8b1a7bd02ffad6882019df66de58f5d228
AndreyMelman <lagunmelman@gmail.com>
2024-09-26 15:01:21 +0300

    add to main router api/v1

--- app/main.py
+++ app/main.py
@@ -6,6 +6,9 @@ from contextlib import asynccontextmanager
 from core.config import settings
 from core.db import db_helper
 
+from api import router as api_router
+
+
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     yield
@@ -15,8 +18,10 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(
     lifespan=lifespan,
+    title="ApiTaskManager",
 )
 
+app.include_router(api_router)
 
 if __name__ == "__main__":
     uvicorn.run(
