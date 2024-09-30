47ce4169f82750f70e07a93ed1470c5987938822
AndreyMelman <lagunmelman@gmail.com>
2024-09-30 13:08:59 +0300

    created

--- app/deps/dependencies.py
+++ app/deps/dependencies.py
@@ -6,12 +6,13 @@ from starlette import status
 
 from core.db import db_helper
 from crud import tasks, notes
+from models import Task, Note
 
 
 async def task_by_id(
     task_id: Annotated[int, Path],
     session: AsyncSession = Depends(db_helper.getter_session),
-):
+) -> Task:
     task = await tasks.get_task(task_id=task_id, session=session)
     if task is not None:
         return task
@@ -24,10 +25,10 @@ async def task_by_id(
 async def note_by_id(
     note_id: Annotated[int, Path],
     session: AsyncSession = Depends(db_helper.getter_session),
-):
-    task = await notes.get_note(note_id=note_id, session=session)
-    if task is not None:
-        return task
+) -> Note:
+    note = await notes.get_note(note_id=note_id, session=session)
+    if note is not None:
+        return note
     raise HTTPException(
         status_code=status.HTTP_404_NOT_FOUND,
         detail=f"Task {note_id} not found",
