13c7c361cee65b332d4d09d62be8a2f1acad7a5e
AndreyMelman <lagunmelman@gmail.com>
2024-09-30 13:09:57 +0300

    add endpoint patch and delete

--- app/api/api_v1/notes.py
+++ app/api/api_v1/notes.py
@@ -1,42 +1,54 @@
-from fastapi import APIRouter, Depends, status, HTTPException
+from fastapi import APIRouter, Depends, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
 
 from crud import notes
 from core.db import db_helper
-from schemas.note import Note, NoteCreate
+from schemas.note import Note, NoteCreate, NoteUpdate
 from deps.dependencies import note_by_id
 
 router = APIRouter(tags=["Notes"])
 
 
-@router.get(
-    "/",
-    response_model=list[Note],
-)
+@router.get("/", response_model=list[Note])
 async def get_notes(
     session: AsyncSession = Depends(db_helper.getter_session),
 ):
     return await notes.get_notes(session=session)
 
 
-@router.get(
-    "/{note_id}",
-    response_model=Note,
-)
+@router.get("/{note_id}/", response_model=Note)
 async def get_note(
     note: Note = Depends(note_by_id),
 ):
     return note
 
 
-@router.post(
-    "/",
-    response_model=Note,
-    status_code=status.HTTP_201_CREATED,
-)
+@router.post("/", response_model=Note, status_code=status.HTTP_201_CREATED)
 async def create_note(
     note_in: NoteCreate,
     session: AsyncSession = Depends(db_helper.getter_session),
 ):
     return await notes.create_note(session=session, note_in=note_in)
+
+
+@router.patch("/{note_id}/", response_model=Note)
+async def update_note(
+    note_update: NoteUpdate,
+    note: Note = Depends(note_by_id),
+    session: AsyncSession = Depends(db_helper.getter_session),
+):
+    return await notes.update_note(
+        session=session,
+        note=note,
+        note_update=note_update,
+        partial=True,
+    )
+
+
+@router.delete("/{note_id}/", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_note(
+    note: Note = Depends(note_by_id),
+    session: AsyncSession = Depends(db_helper.getter_session),
+) -> None:
+    return await notes.delete_note(session=session, note=note)
--- app/api/api_v1/tasks.py
+++ app/api/api_v1/tasks.py
@@ -1,41 +1,59 @@
-from fastapi import APIRouter, Depends, status, HTTPException
+from fastapi import APIRouter, Depends, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from core.db import db_helper
 from crud import tasks
-from schemas.task import Task, TaskCreate
+from schemas.task import Task, TaskCreate, TaskUpdate
 from deps.dependencies import task_by_id
 
 router = APIRouter(tags=["Tasks"])
 
 
-@router.get(
-    "/",
-    response_model=list[Task],
-)
+@router.get("/", response_model=list[Task])
 async def get_tasks(
     session: AsyncSession = Depends(db_helper.getter_session),
 ):
     return await tasks.get_tasks(session=session)
 
 
-@router.get(
-    "/{task_id}",
-    response_model=Task,
-)
+@router.get("/{task_id}/", response_model=Task)
 async def get_task(
     task: Task = Depends(task_by_id),
 ):
     return task
 
 
-@router.post(
-    "/",
-    response_model=Task,
-    status_code=status.HTTP_201_CREATED,
-)
+@router.post("/", response_model=Task, status_code=status.HTTP_201_CREATED)
 async def create_task(
     task_in: TaskCreate,
     session: AsyncSession = Depends(db_helper.getter_session),
 ):
-    return await tasks.create_task(session=session, task_in=task_in)
+    return await tasks.create_task(
+        session=session,
+        task_in=task_in,
+    )
+
+
+@router.patch("/{task_id}/", response_model=Task)
+async def update_task(
+    task_update: TaskUpdate,
+    session: AsyncSession = Depends(db_helper.getter_session),
+    task: Task = Depends(task_by_id),
+):
+    return await tasks.update_task(
+        session=session,
+        task=task,
+        task_update=task_update,
+        partial=True,
+    )
+
+
+@router.delete("/{task_id}/", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_task(
+    session: AsyncSession = Depends(db_helper.getter_session),
+    task: Task = Depends(task_by_id),
+):
+    return await tasks.delete_task(
+        session=session,
+        task=task,
+    )
