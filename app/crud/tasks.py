8cfaed1f77af726b7f1faf76e8777844c8c40179
AndreyMelman <lagunmelman@gmail.com>
2024-10-01 16:06:40 +0300

    crud/task add func filter update, priority update, mark completed

--- app/crud/tasks.py
+++ app/crud/tasks.py
@@ -2,13 +2,35 @@ from sqlalchemy import select, Result
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from models import Task
-from schemas.task import TaskCreate, TaskUpdate
+from models.task import PriorityEnum
+from schemas.task import (
+    TaskCreate,
+    TaskUpdate,
+    TaskUpdateStatus,
+    TaskUpdatePriority,
+)
 
 
 async def get_tasks(
     session: AsyncSession,
 ) -> list[Task]:
     stmt = select(Task).order_by(Task.id)
+    result: Result = await session.execute(stmt)
+    tasks = result.scalars().all()
+    return list(tasks)
+
+async def get_filter_tasks(
+    session: AsyncSession,
+    priority: PriorityEnum,
+    completed: bool,
+) -> list[Task]:
+    stmt = select(Task).order_by(Task.id)
+
+    if priority:
+        stmt = stmt.filter(Task.priority == priority)
+    if completed:
+        stmt = stmt.filter(Task.completed == completed)
+
     result: Result = await session.execute(stmt)
     tasks = result.scalars().all()
     return list(tasks)
@@ -49,3 +71,25 @@ async def delete_task(
 ) -> None:
     await session.delete(task)
     await session.commit()
+
+
+async def mark_task_as_completed(
+    session: AsyncSession,
+    task_update_status: TaskUpdateStatus,
+    task: Task,
+) -> Task | None:
+    task.completed = task_update_status.completed
+    await session.commit()
+    return task
+
+
+async def update_task_priority(
+    session: AsyncSession,
+    task: Task,
+    priority_name: PriorityEnum,
+) -> Task:
+    task.priority = priority_name
+
+    await session.commit()
+    await session.refresh(task)
+    return task
