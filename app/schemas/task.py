49c768eb42949139a556710a7059c2f84046296c
AndreyMelman <lagunmelman@gmail.com>
2024-10-01 16:04:06 +0300

    add in schemas/task update priority and status

--- app/schemas/task.py
+++ app/schemas/task.py
@@ -2,12 +2,14 @@ from datetime import datetime
 
 from pydantic import BaseModel, ConfigDict, Field
 
+from models.task import PriorityEnum
+
 
 class TaskBase(BaseModel):
-    title: str
-    description: str
-    priority: str
-    deadline_at: datetime
+    title: str = Field(default="", min_length=1, max_length=50)
+    description: str = Field(default="", max_length=50000)
+    priority: PriorityEnum = PriorityEnum.low
+    deadline_at: datetime = Field(default=datetime.now())
     completed: bool = Field(default=False)
 
 
@@ -16,11 +18,19 @@ class TaskCreate(TaskBase):
 
 
 class TaskUpdate(TaskCreate):
-    title: str | None = None
-    description: str | None = None
-    priority: str | None = None
-    deadline_at: datetime | None = None
-    completed: bool | None = None
+    title: str | None = Field(default="", max_length=50)
+    description: str | None = Field(default="", max_length=50000)
+    priority: PriorityEnum | None = None
+    deadline_at: datetime | None = Field(default=datetime.now())
+    completed: bool | None = Field(default=False)
+
+
+class TaskUpdateStatus(BaseModel):
+    completed: bool = Field(default=True)
+
+
+class TaskUpdatePriority(BaseModel):
+    priority: PriorityEnum
 
 
 class Task(TaskBase):
