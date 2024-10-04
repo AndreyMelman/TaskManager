b54ad08a5805f28041df965f2f0941ba80d2a644
AndreyMelman <lagunmelman@gmail.com>
2024-10-04 16:01:11 +0300

    add migration for users table

--- app/alembic/env.py
+++ app/alembic/env.py
@@ -8,7 +8,7 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from core.config import settings
 from core.db import Base
-from models import User, Task, Note  # noqa
+from models import User # noqa
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
