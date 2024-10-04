37ac47eb88affbe58beb4f1bc358bb57d7ddac8c
AndreyMelman <lagunmelman@gmail.com>
2024-10-04 16:49:54 +0300

    create access_token migration

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -3,9 +3,11 @@ __all__ = (
     # "Note",
     # "Profile",
     "User",
+    "AccessToken",
 )
 
 # from .task import Task
 # from .note import Note
 # from .profile import Profile
 from .user import User
+from .access_token import AccessToken
