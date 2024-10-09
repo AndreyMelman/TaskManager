0b5bffb39edef6de4bc3623d2be8886336662059
AndreyMelman <lagunmelman@gmail.com>
2024-10-09 16:10:56 +0300

    update schemas/note

--- app/schemas/note.py
+++ app/schemas/note.py
@@ -1,11 +1,12 @@
 from datetime import datetime
 
-from pydantic import BaseModel, ConfigDict
+from pydantic import BaseModel, ConfigDict, Field
 
 
 class NoteBase(BaseModel):
-    title: str
-    content: str
+    title: str = Field(default="", min_length=1, max_length=50)
+    content: str = Field(default="", min_length=0, max_length=50000)
+
 
 
 class NoteCreate(NoteBase):
@@ -13,8 +14,8 @@ class NoteCreate(NoteBase):
 
 
 class NoteUpdate(NoteCreate):
-    title: str | None = None
-    content: str | None = None
+    title: str | None = Field(default="", max_length=50)
+    content: str | None = Field(default="", max_length=50000)
 
 
 class Note(NoteBase):
@@ -23,3 +24,4 @@ class Note(NoteBase):
     created_at: datetime
     updated_at: datetime
     id: int
+    user_id: int
