e00bbf297ae77765fe48a758f99dd980e22c4b49
AndreyMelman <lagunmelman@gmail.com>
2024-10-14 17:17:41 +0300

    move smtp_service.py to utils package

--- app/core/authentication/user_manager.py
+++ app/core/authentication/user_manager.py
@@ -3,7 +3,7 @@ from typing import Optional, TYPE_CHECKING, Any
 
 from fastapi_users import BaseUserManager, IntegerIDMixin
 
-from mailer.smtp_service import send_email
+from utils.smtp_service import send_email
 from models import User
 from core.config import settings
 from core.types.user_id import UserIdType
