d7a1dfa742d6701b23ab06b4afa6e6fe91146033
AndreyMelman <lagunmelman@gmail.com>
2024-10-17 01:09:25 +0300

    add Api v1 prefix categories

--- app/core/config.py
+++ app/core/config.py
@@ -14,6 +14,7 @@ class ApiV1Prefix(BaseModel):
     auth: str = "/auth"
     users: str = "/users"
     profiles: str = "/profiles"
+    categories: str = "/categories"
 
 
 class ApiPrefix(BaseModel):
