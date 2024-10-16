84cf16eee5ff18921a7177afdcbc82fa7f970bde
AndreyMelman <lagunmelman@gmail.com>
2024-10-16 15:56:04 +0300

    add annotated created_at and updated_at

--- app/core/db/base.py
+++ app/core/db/base.py
@@ -1,8 +1,15 @@
-from sqlalchemy import MetaData
-from sqlalchemy.orm import DeclarativeBase, declared_attr
+from datetime import datetime
+from typing import Annotated
+
+from sqlalchemy import MetaData, func
+from sqlalchemy.orm import DeclarativeBase, declared_attr, mapped_column
 
 from core.config import settings
 from utils.case_converter import camel_case_to_snake_case
+
+
+created_at = Annotated[datetime, mapped_column(server_default=func.now())]
+updated_at = Annotated[datetime, mapped_column(server_default=func.now(), onupdate=datetime.now)]
 
 
 class Base(DeclarativeBase):
