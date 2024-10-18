6519d51f78c162e7af0af0939f0b99787169a977
AndreyMelman <lagunmelman@gmail.com>
2024-10-19 01:46:46 +0300

    create celery_app for send mail

---
+++ app/celery_app/celery_app.py
@@ -0,0 +1,45 @@
+import smtplib
+import logging
+
+from core.config import settings
+
+from celery import Celery
+
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+log = logging.getLogger(__name__)
+
+celery_app = Celery(
+    "notification",
+    broker=settings.celery.celery_broker_url,
+    backend=settings.celery.celery_result_backend,
+)
+
+celery_app.conf.update(
+    broker_connection_retry_on_startup=True,
+)
+
+
+@celery_app.task
+def send_mail(
+    to_email: str,
+    subject: str,
+    body: str,
+):
+    message = MIMEMultipart()
+    message["From"] = settings.email_a.smtp_user
+    message["To"] = to_email
+    message["Subject"] = subject
+    message.attach(MIMEText(body, "plain"))
+
+    try:
+        with smtplib.SMTP(
+            settings.email_a.smtp_server, settings.email_a.smtp_port
+        ) as server:
+            server.starttls()
+            server.login(settings.email_a.smtp_user, settings.email_a.smtp_password)
+            server.sendmail(settings.email_a.smtp_user, to_email, message.as_string())
+
+    except Exception as e:
+        log.error("Failed to send email to %r: %r", to_email, e)
