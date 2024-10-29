305ce26ea75ccb6cab0c2f2e1c61ac0f4b075a41
AndreyMelman <lagunmelman@gmail.com>
2024-10-29 15:27:31 +0300

    fix problems

--- tests/test_modules/test_tasks.py
+++ tests/test_modules/test_tasks.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
 import pytest
-from pydantic import ValidationError
+
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from crud.tasks import create_task
@@ -10,27 +10,112 @@ from schemas.task import TaskCreate
 
 from contextlib import nullcontext as does_not_raise
 
+
 @pytest.mark.asyncio
 @pytest.mark.parametrize(
     "title, description, priority, deadline_at, completed, expected",
     [
-        ("Test Task", "This is a test task", "Low", "2024-10-25T19:47", False, does_not_raise()),
-        ("Test Task", None, "Low", "2024-10-25T19:47", False, does_not_raise()),
-        ("Test Task", "This is a test task", "Medium", "2024-10-25T19:47", False, does_not_raise()),
-        ("Test Task", "This is a test task", "Low", None, False, does_not_raise()),
-        ("Test Task", "This is a test task", "Low", "2024-10-25T19:47", True, does_not_raise()),
-        (1, "This is a test task", "Low", "2024-10-25T19:47", False, pytest.raises(ValueError)),
-        ("Test Task", 1, "Low", "2024-10-25T19:47", False, pytest.raises(ValueError)),
-        ("Test Task", "This is a test task", 1, "2024-10-25T19:47", False, pytest.raises(ValueError)),
-        ("Test Task", "This is a test task", "Q", "2024-10-25T19:47", False, pytest.raises(ValueError)),
-        ("Test Task", "This is a test task", 1, "2024-10-25T19:47", False, pytest.raises(ValueError)),
-        ("Test Task", "This is a test task", False, "2024-10-25T19:47", False, pytest.raises(ValueError)),
-        ("Test Task", "This is a test task", None, "2024-10-25T19:47", False, pytest.raises(ValueError)),
-
+        (
+            "Test Task1",
+            "This is a test task",
+            "Low",
+            "2024-10-25T19:47",
+            False,
+            does_not_raise(),
+        ),
+        (
+            "Test Task2",
+            None,
+            "Low",
+            "2024-10-25T19:47",
+            False,
+            does_not_raise(),
+        ),
+        (
+            "Test Task3",
+            "This is a test task",
+            "Medium",
+            "2024-10-25T19:47",
+            False,
+            does_not_raise(),
+        ),
+        (
+            "Test Task4",
+            "This is a test task",
+            "Low",
+            None,
+            False,
+            does_not_raise(),
+        ),
+        (
+            "Test Task5",
+            "This is a test task",
+            "Low",
+            "2024-10-25T19:47",
+            True,
+            does_not_raise(),
+        ),
+        (
+            1,
+            "This is a test task",
+            "Low",
+            "2024-10-25T19:47",
+            False,
+            pytest.raises(ValueError),
+        ),
+        (
+            "Test Task7",
+            1,
+            "Low",
+            "2024-10-25T19:47",
+            False,
+            pytest.raises(ValueError),
+        ),
+        (
+            "Test Task8",
+            "This is a test task",
+            1,
+            "2024-10-25T19:47",
+            False,
+            pytest.raises(ValueError),
+        ),
+        (
+            "Test Task9",
+            "This is a test task",
+            "Q",
+            "2024-10-25T19:47",
+            False,
+            pytest.raises(ValueError),
+        ),
+        (
+            "Test Task10",
+            "This is a test task",
+            1,
+            "2024-10-25T19:47",
+            False,
+            pytest.raises(ValueError),
+        ),
+        (
+            "Test Task11",
+            "This is a test task",
+            False,
+            "2024-10-25T19:47",
+            False,
+            pytest.raises(ValueError),
+        ),
+        (
+            "Test Task12",
+            "This is a test task",
+            None,
+            "2024-10-25T19:47",
+            False,
+            pytest.raises(ValueError),
+        ),
     ],
 )
 async def test_greate_task(
     session: AsyncSession,
+    test_user: User,
     title: str,
     description: str,
     priority: str,
@@ -38,14 +123,6 @@ async def test_greate_task(
     completed: bool,
     expected,
 ):
-
-    test_user = User(
-        email="test@example.com",
-        hashed_password="pass",
-    )
-    session.add(test_user)
-    await session.commit()
-    await session.refresh(test_user)
     with expected:
         task_data = TaskCreate(
             title=title,
@@ -64,5 +141,3 @@ async def test_greate_task(
         assert new_task.deadline_at == task_data.deadline_at
         assert new_task.completed == task_data.completed
         assert new_task.user_id == test_user.id
-
-
