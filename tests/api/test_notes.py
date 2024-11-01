4f71b7d2e116cab45d6b3a1441aa35970a539a8f
AndreyMelman <lagunmelman@gmail.com>
2024-11-01 17:40:33 +0300

    add api/test_notes tests

--- tests/api/test_notes.py
+++ tests/api/test_notes.py
@@ -1,23 +1,56 @@
 import pytest
+
 from httpx import AsyncClient, ASGITransport
+from sqlalchemy.ext.asyncio import AsyncSession
 
-from core.config import settings
 from main import app
-from models import User
-from tests.utils.utils import get_superuser_token_headers
+from models import User, Note
+from tests.utils.note import create_random_note
 
 
-@pytest.mark.asyncio
-async def test_create_notes(test_users: User) -> None:
+@pytest.mark.asyncio(loop_scope="session")
+async def test_create_notes(
+    superuser: User,
+    superuser_token_headers: dict[str, str],
+) -> None:
     data = {"title": "Foo", "content": "Fighters"}
-    headers = await get_superuser_token_headers()
     async with AsyncClient(
         transport=ASGITransport(app=app),
         base_url="http://localhost:8080",
     ) as client:
         response = await client.post(
             "/api/v1/notes/",
-            headers=headers,
+            headers=superuser_token_headers,
             json=data,
         )
     assert response.status_code == 201
+    content = response.json()
+    assert content["title"] == data["title"]
+    assert content["content"] == data["content"]
+    assert "id" in content
+    assert "created_at" in content
+    assert "updated_at" in content
+
+
+@pytest.mark.asyncio(loop_scope="session")
+async def test_get_note(
+    session: AsyncSession,
+    superuser: User,
+    superuser_token_headers: dict[str, str],
+) -> None:
+    note = await create_random_note(session=session, superuser=superuser)
+    async with AsyncClient(
+        transport=ASGITransport(app=app), base_url="http://localhost:8080"
+    ) as client:
+        response = await client.get(
+            f"/api/v1/notes/{note.id}/",
+            headers=superuser_token_headers,
+            params={"item_id": note.id},
+        )
+
+    assert response.status_code == 200
+    content = response.json()
+    assert content["title"] == note.title
+    assert content["content"] == note.content
+    assert content["id"] == note.id
+    assert content["user_id"] == superuser.id
