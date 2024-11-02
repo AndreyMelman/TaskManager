532782e506bcff6b0644c7473066d658b2f4cddc
AndreyMelman <lagunmelman@gmail.com>
2024-11-02 14:41:47 +0300

    add test get not found

--- tests/api/test_notes.py
+++ tests/api/test_notes.py
@@ -1,3 +1,5 @@
+import random
+
 import pytest
 
 from httpx import AsyncClient, ASGITransport
@@ -40,7 +42,8 @@ async def test_get_note(
 ) -> None:
     note = await create_random_note(session=session, superuser=superuser)
     async with AsyncClient(
-        transport=ASGITransport(app=app), base_url="http://localhost:8080"
+        transport=ASGITransport(app=app),
+        base_url="http://localhost:8080",
     ) as client:
         response = await client.get(
             f"/api/v1/notes/{note.id}/",
@@ -54,3 +57,24 @@ async def test_get_note(
     assert content["content"] == note.content
     assert content["id"] == note.id
     assert content["user_id"] == superuser.id
+
+
+@pytest.mark.asyncio(loop_scope="session")
+async def test_get_note_not_found(
+    session: AsyncSession,
+    superuser: User,
+    superuser_token_headers: dict[str, str],
+):
+    note_id = random.randint(1, 100)
+    async with AsyncClient(
+        transport=ASGITransport(app=app),
+        base_url="http://localhost:8080",
+    ) as client:
+        response = await client.get(
+            f"/api/v1/notes/{note_id}/",
+            headers=superuser_token_headers,
+            params={"item_id": note_id},
+        )
+    assert response.status_code == 404
+    content = response.json()
+    assert content["detail"] == f"Note {note_id} not found"
